afe276c8172fb8d5fcddd0113afbe07637762a7a
Funes <lucas.martin.funes@accenture.com>
2018-06-07 16:33:15 -0300

    Ejercicio 1

---
+++ Clase 01/script.js
@@ -0,0 +1,17 @@
+var number1 = prompt('Escribí el primer número:', 'Ej: 8')
+
+var parsedNumber1 = parseFloat(number1, 10)
+
+var number2 = prompt('Escribí el segundo número:', 'Ej: 8')
+
+var parsedNumber2 = parseFloat(number2, 10)
+
+alert('La suma de los dos números es: ' + (parsedNumber1 + parsedNumber2) + ' !')
+
+alert('La resta de los dos números es: ' + (parsedNumber1 - parsedNumber2) + ' !')
+
+alert('La multiplicación de los dos números es: ' + parsedNumber1 * parsedNumber2 + ' !')
+
+alert('La división de los dos números es: ' + parsedNumber1 / parsedNumber2 + ' !')
+
+alert('Completaste el ejercicio !')
