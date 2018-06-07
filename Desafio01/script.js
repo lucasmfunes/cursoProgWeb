019c9e68247b4213419aeaabec8c783e88e8188e
Funes <lucas.martin.funes@accenture.com>
2018-06-07 16:37:35 -0300

    Ejercicio 1 terminado

---
+++ Desafio01/script.js
@@ -0,0 +1,16 @@
+var numero1 = prompt('Coloca un numero:', 'Ejemplo 4')
+
+var parsedNumero1 = parseFloat(numero1, 10)
+
+var numero2 = prompt('Coloca otro:', 'Ej: 8')
+
+var parsedNumero2 = parseFloat(numero2, 10)
+
+alert('La suma de tus numeros es: ' + (parsedNumero1 + parsedNumero2) + ' !')
+
+alert('La resta de tus numeros es: ' + (parsedNumero1 - parsedNumero2) + ' !')
+
+alert('La multiplicación de tus numeros es: ' + parsedNumero1 * parsedNumero2 + ' !')
+
+alert('La división de tus numeros es: ' + parsedNumero1 / parsedNumero2 + ' !')
+
